424fedf7b2e3112b681464c68bef4aa927aa26de
粘光裕 <tgnivekucn@gmail.com>
2018-11-05 21:52:45 +0800

    first commit

---
+++ GetLocation/ViewController.swift
@@ -0,0 +1,62 @@
+//
+//  ViewController.swift
+//  GetLocation
+//
+//  Created by 粘光裕 on 2018/11/5.
+//  Copyright © 2018年 com.open.lib. All rights reserved.
+//
+
+import UIKit
+import CoreLocation
+class ViewController: UIViewController {
+    let locationManager = CLLocationManager()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        locationManager.delegate = self //每次設定CLLocationManager的delegate,都會執行到didChangeAuthorization這個system callback
+    }
+
+    
+    @IBAction func requestAuthorization(_ sender: UIButton) {
+        locationManager.requestWhenInUseAuthorization()
+    }
+    
+    
+}
+
+extension ViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        var statusString = ""
+        switch status {
+        case .authorizedAlways:
+            statusString = "authorizedAlways"
+            break
+        case .authorizedWhenInUse:
+            statusString = "authorizedWhenInUse"
+            break
+        case .denied:
+            statusString = "denied"
+            break
+        case .notDetermined:
+            statusString = "notDetermined"
+            break
+        case .restricted:
+            statusString = "restricted"
+            break
+        }
+        if status != .notDetermined {
+            showCustomAlert()
+        }
+        print("didChangeAuthorization  status is: \(statusString)")
+    }
+    
+    func showCustomAlert() {
+        let alert = UIAlertController(title: "", message: "要求定位權限", preferredStyle: UIAlertController.Style.alert)
+        alert.addAction(UIAlertAction(title: "前往設定頁面", style: .default) {
+            _ in
+            UIApplication.shared.open(URL(string: "App-Prefs:root=com.open.lib.GetLocation")!, options: [:], completionHandler: nil)
+
+            })
+        self.present(alert, animated: true, completion: nil)
+    }
+}
